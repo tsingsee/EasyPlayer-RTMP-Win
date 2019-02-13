8542353ca0775d92eb0b64dddd0af8035ad1c769
kim <ahqjjl@163.com>
2019-02-13 18:24:35 +0800

    update EasyRTMPClient

--- Src/C++/libEasyPlayer/EasyRTMPClient/EasyRTMPClientAPI.h
+++ Src/C++/libEasyPlayer/EasyRTMPClient/EasyRTMPClientAPI.h
@@ -6,28 +6,31 @@
 */
 #ifndef _EasyRTMP_API_H
 #define _EasyRTMP_API_H
-#include <string>
-#include "EasyTypes.h"
-using namespace std;
 
-const int NOT_INIT = -101;//未初始化，调用EasyRTMP_Create EasyRTMP_StartStream 先
-const int URL_EMPTY = -102;//URL为空
-const int URL_SETUP_FAILED = -103;//RTMP Setup失败
-const int RTMP_CONNECT_FAILED = -104;
-const int RTMPSTREAM_CONNECT_FAILED = -105;
-const int RECONNECT_SETUP_FAILED = -106;
-const int RECONNECT_FAILED = -107;
-const int RTMP_UNCONNECTED = -108;
-const int RTMP_RECONNECTED_FAILED = -109;
-const int RTMP_RECV_PARAMERROR = -110;
-const int RTMP_RECONNECT_TIMEOUT = -111;
-const int FRAME_TOO_LEN	= -112;
-const int FRAME_ERROR = -113;//帧错误
-const int SEND_SEQUNECE_FAILED = -114;
-const int READ_BUFFER_FAILED = -115;
-const int READ_DATA_PARAM_ERROR = -116;
-const int RTMP_INSIDE_ERROR = -117;
-const int RTMP_PARSE_FAILED = -118;/*RTMP帧解析失败*/
+#include "EasyTypes.h"
+
+
+enum ErrorType
+{
+    NOT_INIT = -101,
+    URL_EMPTY = -102,
+    URL_SETUP_FAILED = -103,
+    RTMP_CONNECT_FAILED = -104,
+    RTMPSTREAM_CONNECT_FAILED = -105,
+    RECONNECT_SETUP_FAILED = -106,
+    RECONNECT_FAILED = -107,
+    RTMP_UNCONNECTED = -108,
+    RTMP_RECONNECTED_FAILED = -109,
+    RTMP_RECV_PARAMERROR = -110,
+    RTMP_RECONNECT_TIMEOUT = -111,
+    FRAME_TOO_LEN	= -112,
+    FRAME_ERROR = -113,
+    SEND_SEQUNECE_FAILED = -114,
+    READ_BUFFER_FAILED = -115,
+    READ_DATA_PARAM_ERROR = -116,
+    RTMP_INSIDE_ERROR = -117,
+    RTMP_PARSE_FAILED = -118
+};
 
 #define Easy_RTMP_Handle void*
 
@@ -43,34 +46,31 @@ enum MediaType
 	e_MediaType_Flv //flv tag
 };
 
-/*
-	_frameType:		MediaType
-	_frameInfo:		帧结构数据
-	_channelPtr:		用户自定义数据
-*/
+typedef struct __EasyRTMPClientParams_T{
+    bool reconnect; //whether reconnet
+}EasyRTMPClientParams;
+
+
 typedef int (*EasyRTMPClientCallBack)(int _channelId, void* _channelPtr, int _frameType, char *pBuf, EASY_FRAME_INFO* _frameInfo);
 
 #ifdef __cplusplus
 extern "C"
 {
 #endif
-	/* 激活EasyRTMPClient拉流库 */
 #ifdef ANDROID
 	Easy_API Easy_I32 EasyRTMPClient_Activate(char *license, char* userPtr);
 #else
 	Easy_API Easy_I32 EasyRTMPClient_Activate(char *license);
 #endif
 
-	/* 创建EasyRTMPClient句柄  返回为句柄值 */
 	Easy_API Easy_RTMP_Handle EasyRTMPClient_Create();
 
-	/* 释放EasyRTMPClient 参数为EasyRTMP句柄 */
 	Easy_API int EasyRTMPClient_Release(Easy_RTMP_Handle handle);
 
-	/* 设置数据回调 */
 	Easy_API int EasyRTMPClient_SetCallback(Easy_RTMP_Handle handle, EasyRTMPClientCallBack _callback);
 
-	/* 打开网络流(拉取或者推送) */
+    Easy_API int EasyRTMPClient_SetParams(Easy_RTMP_Handle handle, EasyRTMPClientParams* param);
+
 	Easy_API int EasyRTMPClient_StartStream(Easy_RTMP_Handle handle, int _channelid, const char* _url, void* _channelPtr);
 #ifdef __cplusplus
 }
