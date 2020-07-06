e59f48d99ce5de7c6501f669deed3d30a158f54f
Seven <seven@easydarwin.org>
2020-07-06 09:41:58 +0800

    update KEY

--- Src/C#/EasyPlayer-RTMP/EasyPlayer-RTMP.NetSDK/PlayerSdk.cs
+++ Src/C#/EasyPlayer-RTMP/EasyPlayer-RTMP.NetSDK/PlayerSdk.cs
@@ -130,7 +130,7 @@ namespace EasyPlayer_RTMP.NetSDK
         /// </summary>
         /// <returns>System.Int32.</returns>
         [DllImport(@"Lib\libEasyPlayer-RTMP.dll", CallingConvention = CallingConvention.Cdecl, EntryPoint = "?EasyPlayer_Init@@YAHPAD@Z")]
-        public static extern int EasyPlayer_Init(string key = "59615A67426F69576B5A754132594E656F58776D4A65784659584E3555477868655756794C564A55545641755A58686C567778576F502B6C2F69426C59584E35");
+        public static extern int EasyPlayer_Init(string key = "59615A67426F69576B5A75416C2F4A656F51316F5065784659584E3555477868655756794C564A55545641755A58686C567778576F502B392F32566863336B3D");
 
         /// <summary>
         /// 资源释放.
